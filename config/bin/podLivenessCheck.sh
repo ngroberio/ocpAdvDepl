0907196a607ee7bdb2331e26629e0a8476503168
Nilton Groberio <ngroberio@gmail.com>
2018-08-16 12:16:06 +0100

    + UPDATE LIVENES CHECKS

--- config/bin/podLivenessCheck.sh
+++ config/bin/podLivenessCheck.sh
@@ -2,9 +2,9 @@
 echo ">> Liveness Check for Pod ${1} from Project ${2}"
 sleep $3
 while : ; do
-  echo ">> Check Pod ${1} is Ready..."
+  echo ">>>>> CHECK IF POD ${1} IS READY..."
   oc get pod -n $2 | grep $1 | grep -v build | grep -v deploy |grep "1/1.*Running"
   [[ "$?" == "1" ]] || break
-  echo "...no. Sleeping ${3} seconds."
+  echo "NOT YET :( - WAITING FOR MORE ${3} SECONDS TO RETRY."
   sleep $3
 done
