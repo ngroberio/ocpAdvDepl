5324b35446b5c3252eef22c22b1d6973f3fd16c0
Nilton Groberio <ngroberio@gmail.com>
2018-08-17 18:56:54 +0100

    + Separate hosts initiation file

--- install.sh
+++ install.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-while getopts g:i:e:p: option
-do
-case "${option}"
-in
-g) GUID=${OPTARG};;
-i) INTERNAL=${OPTARG};;
-e) EXTERNAL=${OPTARG};;
-p) CURRENT_PATH=${OPTARG};;
-esac
-done
-
-if [ -z $GUID ]; then GUID=`hostname|awk -F. '{print $2}'`; fi
-if [ -z $INTERNAL ]; then INTERNAL=internal; fi
-if [ -z $EXTERNAL ]; then EXTERNAL=example.opentlc.com; fi
-if [ -z $CURRENT_PATH ]; then CURRENT_PATH=`pwd`; fi
-
-echo -- GUID = $GUID --
-echo -- Internal domain = $INTERNAL --
-echo -- External domain = $EXTERNAL --
-echo -- Current path = $CURRENT_PATH --
-
-
 echo  ">>> PREPARING HOSTS FILES"
-cat ./config/templates/hosts_template.yaml | sed -e "s:{GUID}:$GUID:g;s:{DOMAIN_INTERNAL}:$INTERNAL:g;s:{DOMAIN_EXTERNAL}:$EXTERNAL:g;s:{PATH}:$CURRENT_PATH:g;" > hosts
+./config/bin/initiateHostsTemplate.sh
 echo  "<<< PREPARING HOSTS FILES DONE"
 
 echo ">>> INSTALL ATOMIC PACKAGES"
