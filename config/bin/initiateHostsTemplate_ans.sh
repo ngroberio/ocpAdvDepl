e44331190f1583250e7aff47fe149578184cb09d
Nilton Groberio <ngroberio@gmail.com>
2018-08-20 16:27:57 +0100

    UPDATE

--- config/bin/initiateHostsTemplate_ans.sh
+++ config/bin/initiateHostsTemplate_ans.sh
@@ -7,7 +7,7 @@ echo -- GUID = $GUID --
 echo -- Internal domain = $INTERNAL --
 echo -- External domain = $EXTERNAL --
 echo  ">>> PREPARING HOSTS FILES"
-cat /root/ocpAdvDepl/config/templates/hosts_template_ans.yaml | sed -e "s:{GUID}:$GUID:g;s:{DOMAIN_INTERNAL}:$INTERNAL:g;s:{DOMAIN_EXTERNAL}:$EXTERNAL:g;s:{PATH}:$CURRENT_PATH:g;" > hosts
+cat /root/ocpAdvDepl/config/templates/hosts_template_ans.yaml | sed -e "s:{GUID}:$GUID:g;s:{DOMAIN_INTERNAL}:$INTERNAL:g;s:{DOMAIN_EXTERNAL}:$EXTERNAL:g;" > hosts
 echo  "<<< PREPARING HOSTS FILES DONE"
 
 echo  ">>> COPY NEW HOSTS TO ANSIBLE HOSTS"
