77a0f0b7ee0e97a398681b10b7abcc6310c4de38
Nilton Groberio <ngroberio@gmail.com>
2018-08-28 13:26:54 +0100

    + Update create jenkins

--- config/infra/setup_cicd.sh
+++ config/infra/setup_cicd.sh
@@ -10,7 +10,8 @@ echo "<<< SETUP AMY CICD SIMPLE PIPELINE DONE"
 
 echo ">>> SETUP JENKINS"
 #oc new-app jenkins-persistent -p ENABLE_OAUTH=true -e JENKINS_PASSWORD=jenkins -n os-tasks-${GUID}-dev
-oc create -f ./config/templates/setup_jenkins.yaml -n os-tasks-${GUID}-dev
+#oc create -f ./config/templates/setup_jenkins.yaml -n os-tasks-${GUID}-dev
+oc new-app -f -f ./config/templates/setup_jenkins.yaml -p ENABLE_OAUTH=true -e JENKINS_PASSWORD=jenkins -n os-tasks-${GUID}-dev
 
 echo ">>>>> ADD JENKINS USER PERMISSIONS TO SERVICEACCOUNT"
 oc policy add-role-to-user edit system:serviceaccount:os-tasks-${GUID}-dev:jenkins -n os-tasks-${GUID}-dev
