45079fb67ebb286091c71e8ee216b6f5e03a1e14
Nilton Groberio <ngroberio@gmail.com>
2018-09-21 09:58:21 +0100

    +Update users creation

--- config/bin/addIdentityProvider.sh
+++ config/bin/addIdentityProvider.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-htpasswd -c -b /etc/origin/htpasswd amy r3dh4t1!
+htpasswd -b /etc/origin/htpasswd amy r3dh4t1!
 htpasswd -b /etc/origin/htpasswd andrew r3dh4t1!
 htpasswd -b /etc/origin/htpasswd brian r3dh4t1!
 htpasswd -b /etc/origin/htpasswd betty r3dh4t1!
