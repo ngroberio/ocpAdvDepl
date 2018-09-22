078b781b6e6b218945bc33f5f59eb6ce52ec3bad
Nilton Groberio <ngroberio@gmail.com>
2018-09-23 00:10:43 +0100

    ++ Update

--- config/infra/pvs/create_pvs.sh
+++ config/infra/pvs/create_pvs.sh
@@ -5,11 +5,11 @@ mkdir -p /srv/nfs/user-vols/pv{1..200}
 
 echo ">>> CREATE PV FOR USERS"
 
-for pvnum in {1..50} ; do
-  echo "/srv/nfs/user-vols/pv${pvnum} *(rw,all_squash)" >> /etc/exports.d/openshift-uservols.exports
-  chown -R nfsnobody.nfsnobody /srv/nfs
-  chmod -R 777 /srv/nfs
-done
+for pvnum in {1..50}; do echo "/srv/nfs/user-vols/pv${pvnum} *(rw,root_squash)" >> openshift-uservols.exports; done
+sudo cp ./openshift-uservols.exports /etc/exports.d/
+chown -R nfsnobody.nfsnobody /srv/nfs
+chmod -R 777 /srv/nfs
+
 echo "<<< CREATE PV FOR USERS DONE"
 
 echo ">>> RESTARTING NFS SERVER"
