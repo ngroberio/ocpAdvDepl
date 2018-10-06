7c1f0dfe6e88640800bfcfd56f6eeb725d7b9d4f
Nilton Groberio <ngroberio@gmail.com>
2018-10-06 14:02:30 +0100

    +Update messages

--- config/bin/initiateHostsTemplate_ans.sh
+++ config/bin/initiateHostsTemplate_ans.sh
@@ -3,10 +3,12 @@ export GUID=`hostname|awk -F. '{print $2}'`
 echo "export GUID=$GUID" >> $HOME/.bashrc
 export INTERNAL=internal
 export EXTERNAL=example.opentlc.com
+export CURRENT_PATH=`pwd`
 
 echo -- GUID = $GUID --
 echo -- Internal domain = $INTERNAL --
 echo -- External domain = $EXTERNAL --
+echo -- Current path = $CURRENT_PATH --
 echo  ">>> PREPARING HOSTS FILES"
 cat /root/ocpAdvDepl/config/templates/hosts_template_ans.yaml | sed -e "s:{GUID}:$GUID:g;s:{DOMAIN_INTERNAL}:$INTERNAL:g;s:{DOMAIN_EXTERNAL}:$EXTERNAL:g;" > hosts
 echo  "<<< PREPARING HOSTS FILES DONE"
