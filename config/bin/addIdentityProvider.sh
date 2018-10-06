4e71aaac16ced9eac051a1ee7d40a3215f6773e5
Nilton Groberio <ngroberio@gmail.com>
2018-10-06 14:01:30 +0100

    +remove unused users

--- config/bin/addIdentityProvider.sh
+++ config/bin/addIdentityProvider.sh
@@ -3,5 +3,3 @@ htpasswd -b /etc/origin/htpasswd amy r3dh4t1!
 htpasswd -b /etc/origin/htpasswd andrew r3dh4t1!
 htpasswd -b /etc/origin/htpasswd brian r3dh4t1!
 htpasswd -b /etc/origin/htpasswd betty r3dh4t1!
-htpasswd -b /etc/origin/htpasswd cain r3dh4t1!
-htpasswd -b /etc/origin/htpasswd candy r3dh4t1!
