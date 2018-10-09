71546600a7b105064058243addf301c97c604884
Nilton Groberio <ngroberio@gmail.com>
2018-10-09 18:16:34 +0100

    +Update pvs

---
+++ roles/7_CREATE_PVS/files/create_pvs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+export GUID=`hostname | cut -d"." -f2`
+
+mkdir -p /srv/nfs/user-vols/pv{1..200}
+
+echo ">>> CREATE PV FOR USERS"
+
+for pvnum in {1..50}; do echo "/srv/nfs/user-vols/pv${pvnum} *(rw,root_squash)" >> openshift-uservols.exports; done
+sudo cp ./openshift-uservols.exports /etc/exports.d/
+chown -R nfsnobody.nfsnobody /srv/nfs
+chmod -R 777 /srv/nfs
+
+echo "<<< CREATE PV FOR USERS DONE"
+
+echo ">>> RESTARTING NFS SERVER"
+sudo systemctl restart nfs-server
+echo "<<< RESTARTING NFS SERVER DONE"
