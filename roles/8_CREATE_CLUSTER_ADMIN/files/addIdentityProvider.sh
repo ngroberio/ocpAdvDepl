e085bbfea60097c638e1260d9a9642494c8a916e
Nilton Groberio <ngroberio@gmail.com>
2018-10-09 11:19:44 +0100

    +Update roles

--- roles/8_CREATE_CLUSTER_ADMIN/files/addIdentityProvider.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-htpasswd -b /etc/origin/htpasswd amy r3dh4t1!
-htpasswd -b /etc/origin/htpasswd andrew r3dh4t1!
-htpasswd -b /etc/origin/htpasswd brian r3dh4t1!
-htpasswd -b /etc/origin/htpasswd betty r3dh4t1!
