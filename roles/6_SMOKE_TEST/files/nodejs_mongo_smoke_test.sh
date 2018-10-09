618cb23eb1a9a143888e81c022d71eb9d37908b1
Nilton Groberio <ngroberio@gmail.com>
2018-10-09 15:52:30 +0100

    +Update smoke test

--- roles/6_SMOKE_TEST/files/nodejs_mongo_smoke_test.sh
+++ roles/6_SMOKE_TEST/files/nodejs_mongo_smoke_test.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-
 export GUID=`hostname|awk -F. '{print $2}'`
 oc new-project smoke-test-nodejs
 oc new-app nodejs-mongo-persistent
 sleep 70
 oc get pod
 oc get route
-curl -i --head http://nodejs-mongo-persistent-smoke-test.apps.${GUID}.example.opentlc.com
+curl -i --head http://nodejs-mongo-persistent-smoke-test-nodejs.apps.${GUID}.example.opentlc.com
 sleep 30
-oc delete project smoke-test-nodejs
+#oc delete project smoke-test-nodejs
 oc project default
