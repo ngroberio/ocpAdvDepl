f86177844ce296c8a22bb7acd439603223bb7077
Nilton Groberio <ngroberio@gmail.com>
2018-10-11 12:13:50 +0100

    +Add all users

--- roles/11_CREATE_PROJECTS_ROLE/files/add-all-other-users.sh
+++ roles/11_CREATE_PROJECTS_ROLE/files/add-all-other-users.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+htpasswd -b /etc/origin/master/htpasswd admin r3dh4t1!
 htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!
 htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!
 htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!
