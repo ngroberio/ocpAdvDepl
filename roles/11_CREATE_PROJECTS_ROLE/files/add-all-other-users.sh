2e67491a80aea4fd66bbdc0f5c15a63762edd03e
Nilton Groberio <ngroberio@gmail.com>
2018-10-11 10:20:04 +0100

    +remove unused files

--- roles/11_CREATE_PROJECTS_ROLE/files/add-all-other-users.sh
+++ roles/11_CREATE_PROJECTS_ROLE/files/add-all-other-users.sh
@@ -3,6 +3,5 @@ htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!
 htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!
 htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!
 htpasswd -b /etc/origin/master/htpasswd betty r3dh4t1!
-htpasswd -b /etc/origin/master/htpasswd cain r3dh4t1!
-htpasswd -b /etc/origin/master/htpasswd candy r3dh4t1!
-
+htpasswd -b /etc/origin/master/htpasswd nikola r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd tesla r3dh4t1!
--- roles_v1/8_CREATE_CLUSTER_ADMIN/files/addClusterAdmin.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-htpasswd -b /etc/origin/htpasswd admin r3dh4t1!
-oc adm policy add-cluster-role-to-user cluster-admin admin
